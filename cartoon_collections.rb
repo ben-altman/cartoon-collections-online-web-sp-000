28c48f0848341f31ef8620c5e74e3fc3e5f90f98
Ben Altman <altmanguitar@gmail.com>
2020-11-16 04:55:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ def long_planeteer_calls(array)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese.include?(cheese_types)
 end
