c5babd45e0373a2bf8d012ec540cbc3663b45da2
Ben Altman <altmanguitar@gmail.com>
2020-11-16 04:31:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.include? {|e| e.length > 4}
 end
 
 def find_the_cheese# code an argument here
