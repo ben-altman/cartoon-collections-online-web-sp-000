651253b18c199ac27d9e439d32647d461b5d3c4c
Ben Altman <altmanguitar@gmail.com>
2020-11-16 04:43:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.each do |e|
-    if e.length > 4
+  if array.any? do |e|
+    e.length > 4
       return true
     else
       return false
